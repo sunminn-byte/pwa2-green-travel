97eb8483c3fe5be0a3f38b295222b911fed6045f
sunminn-byte <sunminn0627@gamil.com>
2025-09-26 11:38:51 +0900

    250926 css(변수&calc), env 사용

--- src/store/thunks/festivalThunk.js
+++ src/store/thunks/festivalThunk.js
@@ -26,7 +26,9 @@ const festivalIndex = createAsyncThunk(
       // AxiosRequestConfig의 default값 말고 셋팅할 값만 작성
       // headers: {},
       params: {
-        serviceKey: axiosConfig.SERVICE_KEY,
+        // serviceKey: axiosConfig.SERVICE_KEY,
+        serviceKey: import.meta.env.VITE_SERVICE_KEY,
+
         MobileOS: axiosConfig.MOBILE_OS,
         MobileApp: axiosConfig.MOBILE_APP,
         _type: axiosConfig.TYPE,
